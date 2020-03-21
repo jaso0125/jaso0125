4ad108f83d7d66fb7a29b3d4a1093c989ec15c30
uchima <jaso0125@gmail.com>
2020-03-21 13:04:08 +0900

    ToDoList単体テスト前コミット

---
+++ ToDoList/ToDoList/ToDoList/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using ToDoList.Views;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace ToDoList
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            SetMainPage();
+        }
+
+        private static void SetMainPage()
+        {
+            Current.MainPage = new NavigationPage(new MainPage());
+        }
+    }
+}
+
---
+++ ToDoList/ToDoList/ToDoList/Views/SettingPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ToDoList.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SettingPage : ContentPage
+    {
+        public SettingPage()
+        {
+            InitializeComponent();
+            InitializeControlers();
+        }
+
+        private void InitializeControlers()
+        {
+            pickOrder.Items.Add("作成順");
+            pickOrder.Items.Add("項目名順");
+            pickOrder.Items.Add("期日順");
+        }
+    }
+}
